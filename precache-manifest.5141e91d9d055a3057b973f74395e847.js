d01323c4db07d92232c3541cbf294e63f3dbec7d
Thisseas X <thisseasx@yahoo.com>
2020-02-23 02:19:42 +0200

    Updates

--- precache-manifest.5141e91d9d055a3057b973f74395e847.js
+++ precache-manifest.5141e91d9d055a3057b973f74395e847.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "193c7ac69ce2e37fbc430b360cd6638d",
+    "revision": "ba9c01431259b0c12fd6108ce0bdb519",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "3a83e2194943bb64bb83",
-    "url": "/tic-tac-toe/static/css/main.87f62ebe.chunk.css"
+    "revision": "9d85d55c098dc5fae803",
+    "url": "/tic-tac-toe/static/css/main.bc21e856.chunk.css"
   },
   {
     "revision": "30057bf8f359d7faf568",
     "url": "/tic-tac-toe/static/js/2.2bc3b163.chunk.js"
   },
   {
-    "revision": "3a83e2194943bb64bb83",
-    "url": "/tic-tac-toe/static/js/main.42f40f7c.chunk.js"
+    "revision": "9d85d55c098dc5fae803",
+    "url": "/tic-tac-toe/static/js/main.2a68ff47.chunk.js"
   },
   {
     "revision": "e97e367d92ba91a08ca0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe/precache-manifest.606941207d5e1888087c0784e4e01db6.js"
+  "/tic-tac-toe/precache-manifest.5141e91d9d055a3057b973f74395e847.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
